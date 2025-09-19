8143b9170aa217376e63b300d215ad4cb08f40e1
Sebastián López <225sebas@gmail.com>
2025-09-19 15:47:47 -0600

    more functions

--- framework.c
+++ framework.c
@@ -8,12 +8,20 @@ typedef struct Game{
 typedef struct Sprite{
     SDL_Surface* surface;
     SDL_Texture* texture;
-    SDL_Rect* src;
-    SDL_Rect* dest;
+    SDL_Rect src;
+    SDL_Rect dest;
+    Vector2D position;
 }Sprite;
 
+typedef struct Vector2D{
+    int posX;
+    int posY;
+}Vector2D;
+
 // Initializing function for any texture
-int createSprite(Game* game, Sprite* sprite, int x, int y, int w, int h, char* spriteName, int prop1 /* , Filepath*/){
+int createSprite(Game* game, Sprite* sprite, 
+    int x, int y, int src_w, int src_h, char* spriteName, 
+    int prop1 /* , Filepath*/, int dest_h, int dest_w){
     // Load the image into the surface
     sprite->surface = IMG_Load("");
     if(!sprite->surface){
@@ -30,32 +38,27 @@ int createSprite(Game* game, Sprite* sprite, int x, int y, int w, int h, char* s
     SDL_FreeSurface(sprite->surface);
 
     // Set the part of the image we need to copy
-    sprite->src.h = h;
-    sprite->src.w = w;
+    sprite->src.h = src_h;
+    sprite->src.w = src_w;
     sprite->src.x = x;
     sprite->src.y = y;
-
-    // init other properties
-    sprite->sprite_name = spriteName;
-
-    sprite->property_1 = prop1;
-
+    
+    sprite->dest.h = dest_h;
+    sprite->dest.w = dest_w; // Get parameters
     return 0;
 }
 
 // Once we created a sprite, we need so be able to place it
-int placeSprite(Game* game, Sprite* sprite, int h, int w){
+int placeSprite(Game* game, Sprite* sprite){
     if(game->e.type == SDL_MOUSEBUTTONDOWN){
         SDL_GetMouseState(&game->mouse.posm_x,&game->mouse.posm_y);
-        sprite->Dest.x = (game->mouse.posm_x)/TILE_DEST_SIZE; 
-        sprite->Dest.y = (game->mouse.posm_y)/TILE_DEST_SIZE; // spawn sprite at the position where the mouse clicked
+        sprite->dest.x = (game->mouse.posm_x); 
+        sprite->dest.y = (game->mouse.posm_y); // spawn sprite at the position where the mouse clicked
         
-        sprite->Dest.h = h;
-        sprite->Dest.w = w; // Get parameters
         
         // Save where the sprite is when spawned
-        sprite->position.x = (game->mouse.posm_x)/TILE_DEST_SIZE; 
-        sprite->position.y = (game->mouse.posm_y)/TILE_DEST_SIZE; 
+        sprite->position.posX = (game->mouse.posm_x); 
+        sprite->position.posY = (game->mouse.posm_y); 
 
         // We copy it for the render to do the work
         SDL_RenderCopy(game->render, sprite->texture, &sprite->src, &sprite->Dest);
@@ -78,8 +81,8 @@ int updateSprite(Game* game, Sprite* sprite){
     }
 }       
 
-int renderSprite(){
-
+int renderSprite(Game* game){
+    SDL_RenderPresent(game->render);
 }
 
 void destroySprite(){
